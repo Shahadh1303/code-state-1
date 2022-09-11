269b7250556ce4337d77773d2a69ca7d2a278a7d
Your NameR-Lashan <lashan779@gmail.com>
2022-09-11 21:47:22 +0530

    Minor Changes

--- register.js
+++ register.js
@@ -10,6 +10,9 @@ register_btn.addEventListener('click', (e) => {
     e.preventDefault();
     var regex = /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/;
     var password_val = password.value;
+    var usernameDb = localStorage.getItem('Username');
+    var emailDb = localStorage.getItem('Email');
+    var passwordDb = localStorage.getItem('Password')
 
     if(username.value === "" && email.value === "" && password.value === ""){
         username_p.innerHTML = "Username is empty";
@@ -19,6 +22,8 @@ register_btn.addEventListener('click', (e) => {
         email_p.innerHTML = "Invalid Email";
     } else if(password_val.length < 8){
         password_p.innerHTML = "Password shoud be at least of 8 characters";
+    } else if(username.value === usernameDb && email.value === emailDb && password.value === passwordDb){
+        alert("Account already exists")
     } else {
         localStorage.setItem('Username', username.value);
         localStorage.setItem('Email', email.value);
