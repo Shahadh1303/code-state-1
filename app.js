feba0a5c0c9c6e89a7a981f48132fb4c2f6ebab0
Your NameR-Lashan <lashan779@gmail.com>
2022-09-16 17:41:12 +0530

    display username

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-
+var acc_name = localStorage.getItem('Username');
+var dp_acc_name = document.querySelector('.acc-name');
+window.onload = function() {
+    dp_acc_name.innerHTML = `HI ${acc_name},`;
+}
