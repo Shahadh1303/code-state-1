9f3227dbb94004c7c767860db7c9a7636558af31
Your NameR-Lashan <lashan779@gmail.com>
2022-09-16 21:33:26 +0530

    username displayed

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
-var acc_name = localStorage.getItem('Username');
-var dp_acc_name = document.querySelector('.acc-name');
 window.onload = function() {
-    dp_acc_name.innerHTML = `HI ${acc_name},`;
+    var dp_acc_name = document.querySelector('.acc-name');
+    if(localStorage.getItem('Username')){
+        dp_acc_name.innerHTML = `HI ${localStorage.getItem('Username')},`;
+    } else {
+        dp_acc_name.innerHTML = "";
+    }
 }
